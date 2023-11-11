92ce1024d9fc6de2ad301b61a960f82b20eea144
Karen Uzunyan <klaatuka82@gmail.com>
2023-11-11 20:37:57 +0300

    Initial Commit

---
+++ p5m2hm1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  p5m2hm1
+//
+//  Created by Карен Узунян on 11.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
